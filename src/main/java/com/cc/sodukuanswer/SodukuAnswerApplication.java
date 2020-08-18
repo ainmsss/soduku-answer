006d77899a6c5855155a09071363a12052b3c9c6
cuichen <ainmsss.cuichen@gmail.com>
2020-08-18 13:27:13 +0800

    init

---
+++ src/main/java/com/cc/sodukuanswer/SodukuAnswerApplication.java
@@ -0,0 +1,13 @@
+package com.cc.sodukuanswer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SodukuAnswerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SodukuAnswerApplication.class, args);
+    }
+
+}
